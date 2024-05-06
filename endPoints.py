112084cc223d7af1d53db9cba0f7212c504df10c
Mocica Andrei <andrei.mocica@retargeting.biz>
2024-05-06 17:51:16 +0300

    More endpoints

--- endPoints.py
+++ endPoints.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 from PyP100 import PyP110
@@ -10,9 +11,9 @@ app = Flask(__name__)
 @app.route('/switchStatus/<int:status>/<ip>', methods=['POST'])
 def switchStatus(status, ip):
     p110 = PyP110.P110(ip, "email", "pass")
-
     p110.handshake()
     p110.login()
+
     try:
         if status:
             p110.turnOn()
@@ -21,6 +22,27 @@ def switchStatus(status, ip):
         return Response(status=200)
     except:
         return Response(status=500)
+
+
+@app.route('/getDeviceName/<ip>', methods=['GET'])
+def getDeviceName(ip):
+    p110 = PyP110.P110(ip, "email", "pass")
+    p110.handshake()
+    p110.login()
+
+    return jsonify(p110.getDeviceName())
+
+
+@app.route('/getDeviceInfo/<ip>', methods=['GET'])
+def getDeviceInfo(ip):
+    p110 = PyP110.P110(ip, "email", "pass")
+    p110.handshake()
+    p110.login()
+    deviceInfo = p110.getDeviceInfo()
+    deviceInfo['name'] = p110.getDeviceName()
+    print(deviceInfo)
+
+    return jsonify(deviceInfo)
 
 
 if __name__ == '__main__':
